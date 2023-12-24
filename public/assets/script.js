460633f47b26a4382b0f98740a68f6e98cefbfaa
Connor Carciofini <ccarciofini@gmail.com>
2023-12-23 19:18:29 -0600

    table structure

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,8 +1,13 @@
 const doc_main = document.getElementById('main');
-const doc_group_1 = document.getElementById('group_1');
+
+console.log(document.getElementById(`group_${1}`))
 
 function renderElements(element_data) {
     for (let i = 0; i < element_data.length; i++) {
+        if (i === 118) return;
+
+        const current_element = element_data[i];
+
         const card_main = document.createElement('div');
         const card_header = document.createElement('div');
         const card_body = document.createElement('div');
@@ -16,10 +21,10 @@ function renderElements(element_data) {
         const element_symbol = document.createElement('h1');
         const element_name = document.createElement('h2');
 
-        element_number.innerText = `${element_data[i].number}`
-        element_mass.innerText = `${element_data[i].atomic_mass}`;
-        element_symbol.innerText = element_data[i].symbol;
-        element_name.innerText = element_data[i].name;
+        element_number.innerText = `${current_element.number}`
+        element_mass.innerText = `${current_element.atomic_mass}`;
+        element_symbol.innerText = current_element.symbol;
+        element_name.innerText = current_element.name;
 
         card_header.appendChild(element_number);
         card_header.appendChild(element_mass);
@@ -29,7 +34,26 @@ function renderElements(element_data) {
 
         card_main.appendChild(card_header);
         card_main.appendChild(card_body);
-        doc_group_1.appendChild(card_main);
+
+        if(current_element.number >= 57 && current_element.number <= 71){
+            document.getElementById('lanthanides').appendChild(card_main)
+            console.log(current_element.name)
+        } 
+        
+        else if(current_element.number >= 89 && current_element.number <= 103){
+            document.getElementById('actinides').appendChild(card_main)
+        } 
+        
+        else {
+            console.log('bet')
+            const doc_group_to_append = document.getElementById(`group_${current_element.pt_group}`);
+
+            if (current_element.pt_group === 2) doc_group_to_append.classList.add('down_one');
+            if (current_element.pt_group >= 13 && current_element.pt_group <= 17) doc_group_to_append.classList.add('down_one');
+            if (current_element.pt_group >= 3 && current_element.pt_group <= 12) doc_group_to_append.classList.add('down_three');
+
+            doc_group_to_append.appendChild(card_main);
+        }
     };
 };
 
