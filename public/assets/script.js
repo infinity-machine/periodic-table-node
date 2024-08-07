939440452cc8f121310548230c5e6163275e5b12
Connor Carciofini <ccarciofini@gmail.com>
2024-08-06 23:16:34 -0500

    some work

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,24 +1,12 @@
 const main_element = document.getElementById('main');
 const expanded_title = document.getElementById('expanded-title');
-const exp_discovered_by = document.getElementById('exp-discovered_by');
-const expanded_summary = document.getElementById('expanded-summary');
-const element_img = document.getElementById('element-img');
 
-async function handleExpandedView(e) {
-    // const clicked_element_data = e.currentTarget.dataset;
-    // expanded_title.innerText = clicked_element_data.name;
-    // exp_discovered_by.innerText = `Discovered by: ${clicked_element_data.discovered_by}`
-    // expanded_summary.innerText = clicked_element_data.summary;
-    // element_img.setAttribute('src', `${clicked_element_data.bohr_model_image}`);
-};
-
-function renderTable(element_data) {
-    for (let i = 0; i < element_data.length; i++) {
-        if (i === 118) return;
+function renderTable(element_data){
+    for(let i = 0; i < element_data.length; i++){
+        if(i === 118) return;
 
         // HANDLES GENERATION OF CARDS FOR EACH ELEMENT
         const current_element = element_data[i];
-        if(current_element.summary.length > 587) console.log(current_element.name)
 
         const card_main = document.createElement('div');
         const card_header = document.createElement('div');
@@ -55,21 +43,20 @@ function renderTable(element_data) {
 
         card_main.append(card_header);
         card_main.append(card_body);
-        card_main.addEventListener('click', handleExpandedView);
 
         // HANDLES PLACEMENT OF CARDS ON PERIODIC TABLE
-        if (
+        if(
             i < 57
             || (i > 70 && i < 89)
             || i > 102
-        ) {
+        ){
             card_main.classList.add(
                 `row-${current_element.pt_period}`, 
                 `col-${current_element.pt_group}`
             );
         };
 
-        if (i >= 57 && i <= 70) {
+        if(i >= 57 && i <= 70){
             card_main.classList.add(
                 `row-${8}`,
                 `col-${i - 52}`
@@ -77,7 +64,7 @@ function renderTable(element_data) {
             card_main.style.marginTop='2em';
         };
 
-        if (i >= 89 && i <= 102) {
+        if(i >= 89 && i <= 102){
             card_main.classList.add(
                 `row-${9}`,
                 `col-${i - 84}`
@@ -87,41 +74,43 @@ function renderTable(element_data) {
         main_element.append(card_main);
 
         // HANDLES CATEGORY SPECIFIC STYLING OF ELEMENT CARDS
-        if (
+        if(
             current_element.category === 'alkali metal'
         ) card_main.classList.add('alkali');
-        if (
+        if(
             current_element.category === 'alkaline earth metal'
         ) card_main.classList.add('alkaline-earth');
-        if (
+        if(
             current_element.category.includes('transition metal')
         ) card_main.classList.add('trans-metal');
-        if (
+        if(
             current_element.category === 'post-transition metal'
         ) card_main.classList.add('pt-metal');
-        if (
+        if(
             current_element.category === 'metalloid'
         ) card_main.classList.add('metalloid');
-        if (
+        if(
             current_element.category === 'diatomic nonmetal' 
             || current_element.category === 'polyatomic nonmetal'
         ) card_main.classList.add('da-nonmetal');
-        if (
+        if(
             current_element.category === 'noble gas'
         ) card_main.classList.add('noble-gas');
-        if (
+        if(
             current_element.category === 'lanthanide'
         ) card_main.classList.add('lanthanide');
-        if (
+        if(
             current_element.category === 'actinide'
         ) card_main.classList.add('actinide');
     };
 };
 
-async function fetchTableData() {
+async function fetchTableData(){
     const response = await fetch('/elements');
     const element_data = await response.json();
     return element_data;
 };
 
-fetchTableData().then((data) => renderTable(data));
+window.onload = () => {
+    fetchTableData().then((data) => renderTable(data));
+};
